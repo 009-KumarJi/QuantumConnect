a36f0dd99f6f1cf2c72b018938c0f0a26f2c42b8
009-KumarJi <krishnakumar091@yahoo.com>
2024-08-02 02:35:04 +0530

    Started QuantumConnect

---
+++ src/main/java/com/krishna/quantumconnect/QuantumconnectApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.quantumconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishna/quantumconnect/QuantumconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.quantumconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
